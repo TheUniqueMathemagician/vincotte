afc52db9781a3550e77deb761d151d32dba15ec8
Yannick Tamburrini <53182934+TheUniqueMathemagician@users.noreply.github.com>
2024-03-26 18:21:50 +0100

    fix: fix malformed url

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,7 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import { LinksFunction } from "@remix-run/node";
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import { FunctionComponent, PropsWithChildren } from "react";
 
-import styles from "./styles/index.css";
-
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-  { rel: "stylesheet", href: styles },
-];
+import "./styles/index.css";
 
 export const Layout: FunctionComponent<PropsWithChildren> = (props) => {
   const { children } = props;
